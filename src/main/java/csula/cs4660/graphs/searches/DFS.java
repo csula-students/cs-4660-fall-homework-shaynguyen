28bb19a3c74a3efa05fb6c070a2c172a4d0824f5
shay <shay.nguyen@gmail.com>
2016-09-30 23:57:50 -0700

    Removes uses of node.variables and use map/list
    It was diffuclt to keep track of node varibles such as
    node.parent, node.isVisted especially when dealing with
    recursion. Thus i used list and map to keep track of
    visited nodes, and parents nodes. Same effect but
    eaiser for me to follow

--- src/main/java/csula/cs4660/graphs/searches/DFS.java
+++ src/main/java/csula/cs4660/graphs/searches/DFS.java
@@ -3,12 +3,12 @@ package csula.cs4660.graphs.searches;
 import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
+import csula.cs4660.graphs.representations.Representation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.io.File;
+import java.util.*;
 
 /**
  * Created by eric on 9/24/16.
@@ -16,60 +16,71 @@ import java.util.Stack;
 public class DFS implements SearchStrategy {
 
     Log log = LogFactory.getLog(DFS.class);
+    Map<Node, Node> parents;
 
     @Override
     public List<Edge> search(Graph graph, Node source, Node dist) {
         // used to iterate neighbors neighbors neighbors etc ...
         Stack<Node> stack = new Stack<>();
 
+        // init parents
+        parents = new HashMap<>();
+
         // add source, the root node, to the stack
-        source.setVisited(true);
         stack.push(source);
 
         // from the source node in the stack, getPathToDist and find a solution if any.
-        return getPathToDist(graph, stack, new ArrayList<>(), dist);
+        Node goal = findGoal(graph, stack, new ArrayList<>(), dist);
+
+        if(goal == null) return new ArrayList<>();
+        return build(graph,goal);
     }
 
-    private List<Edge> getPathToDist(Graph graph, Stack<Node> stack,
-                                     List<Edge> history, Node dist) {
 
-        // did not find a solution, return an empty list
+    public Node findGoal(Graph graph, Stack<Node> stack, List<Node> visited, Node dist) {
+
+        // depth fully explored, did not find goal
         if (stack.isEmpty())
-            return new ArrayList<>();
+            return null;
 
-        // get the node on top of the stack and get all its neighbors
+        // get all the neighbors of the node on top of the stack
         Node current = stack.peek();
         List<Node> neighbors = graph.neighbors(current);
 
 
-        // if the node is the goal, add it to history and return history
-        // otherwise recursively check its node neighbor
+        // if a node is the dist, then return that node else recursively search
+        // each node neighbors
         for (Node node : neighbors) {
             if (node.equals(dist)) {
-                history.add(new Edge(current, node, graph.distance(current, node)));
-                return history;
-            } else {
-
-                // nodes can share neighbor nodes, ignore the visited ones
-                if (!node.isVisited()) {
-                    stack.add(node);
-
-                    // if the node have a neighbor, the dist could be it, otherwise
-                    // no neighbor indicate no path for this route.
-                    List<Edge> historyCopy = new ArrayList<>(history);
-                    if (!graph.neighbors(node).isEmpty()) {
-                        historyCopy.add(new Edge(current, node, graph.distance(current, node)));
-                    }
-
-                    // getPathToDist  "deeper" into the graph to find the dist node
-                    return getPathToDist(graph, stack, historyCopy, dist);
-                }
+                parents.put(node, current);
+                return node;
+            } else if (!visited.contains(node)) {
+                stack.add(node);
+                parents.put(node, current);
+                return findGoal(graph, stack, visited, dist);
             }
         }
 
-        // there are no (more) neighbors. time to backtrack. set node visited
-        // to true so we don't visit it again as another node's neighbor.
-        stack.pop().setVisited(true);
-        return getPathToDist(graph, stack, history, dist);
+        // the current node has no neighbor or you explored them all, remove it
+        // from the stack and try another node
+        visited.add(stack.pop());
+        return findGoal(graph, stack, visited, dist);
     }
+
+
+    public List<Edge> build(Graph graph, Node goal) {
+        List<Edge> edges = new ArrayList<>();
+
+        // backtrack and add the edges
+        while(parents.get(goal) != null) {
+            edges.add(
+                    new Edge(parents.get(goal), goal, graph.distance(parents.get(goal), goal)));
+            goal = parents.get(goal);
+        }
+
+        // because we build this lsit while back tracking, we need to reverse it
+        Collections.reverse(edges);
+        return edges;
+    }
+
 }
